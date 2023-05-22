54903d11f7f7f0d8e37f2d88274cad23959b65a8
Gashi-JR <130421716+Gashi-JR@users.noreply.github.com>
2023-05-22 15:36:04 +0800

    Delete sy6 directory

--- sy6/src/pages/404.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function w404() {
-    return (
-        <div>404</div>
-    )
-}
--- sy6/src/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-// @ts-ignore
-import { Navigate } from "umi";
-
-export default function index() {
-  return <Navigate to={"/center/home"} />;
-}
